2a6269b45f86bd66154312cef0e26ed8f3d3b7ba
Jakub Żmujdzin <jzmujdzin@gmail.com>
2022-05-25 12:57:58 +0200

    plot position changes, tweaking with radio buttons

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dash import Dash, dcc, html, Input, Output
+import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.graph_objects as go
 import sys, os
@@ -25,11 +26,42 @@ def get_volume_plot():
                              line=dict(color=' #3c3c3d', width=4)), secondary_y=True)
     fig.update_layout(
         plot_bgcolor='white',
-        title='Total Volume and Market Cap'
+        title='Total Volume and Market Cap',
+        legend=dict(
+            yanchor="bottom",
+            y=0.99,
+            xanchor="right",
+            x=0.66)
     )
     fig.update_yaxes(title_text="Volume (USD)", secondary_y=False)
     fig.update_yaxes(title_text="Market Cap (USD)", secondary_y=True)
     return fig
+
+
+def create_radio_table():
+    table_header = [
+        html.Thead(html.Tr([html.Th("Volume"), html.Th("Market Caps")]))
+    ]
+
+    row1 = html.Tr([
+        html.Td(
+            dcc.RadioItems(
+                    ['USD', 'BTC'],
+                    'USD',
+                    id='yaxis-type-volume'
+                )),
+        html.Td(
+            dcc.RadioItems(
+                    ['Perc', 'USD'],
+                    'Perc',
+                    id='yaxis-type-mcap'
+                ))
+        ]
+        )
+
+    table_body = [html.Tbody([row1])]
+
+    return dbc.Table(table_header + table_body, bordered=True)
 
 
 app = Dash(__name__)
@@ -37,47 +69,55 @@ app = Dash(__name__)
 app.layout = html.Div([
         html.H1(children='Crypto Dashboard'),
 
-        dcc.Graph(
-            id='volume',
-            figure=get_volume_plot()
-        ),
+        html.Div([
+            dcc.Graph(
+                id='volume',
+                figure=get_volume_plot()
+            )
 
-        html.H3(children='Display BTC and ETH Market Cap In: ',
-                style={'height': '40px', 'width': '400px', 'margin': 'auto'}),
+        ], style={'height': '25%', 'width': '50%', 'float': 'left'}
+        ),
 
         html.Div([
-            dcc.RadioItems(
-                ['Perc', 'USD'],
-                'Perc',
-                id='yaxis-type',
+            dcc.Graph(
+                id='market-cap'
             )
-        ], style={'height': '40px', 'width': '200px', 'margin': 'auto'}),
 
-        dcc.Graph(
-            id='market-cap'
+        ], style={'height': '25%', 'width': '50%', 'float': 'right'}
         ),
+
+        html.Div(
+            create_radio_table(),
+            style={'align': 'center'}
+        )
+
     ]
 )
 
 
 @app.callback(
     Output('market-cap', 'figure'),
-    Input('yaxis-type', 'value'))
-def get_market_cap_plot(yaxis_type):
+    Input('yaxis-type-mcap', 'value'))
+def get_market_cap_plot(yaxis_type_mcap):
     fig = make_subplots(specs=[[{"secondary_y": True}]])
     fig.add_trace(go.Scatter(x=gcd['timestamp'],
-                             y=(gcd['btc_market_cap_perc'] if yaxis_type == 'Perc' else gcd['btc_market_cap_perc'] * gcd['market_cap_usd']),
+                             y=(gcd['btc_market_cap_perc'] if yaxis_type_mcap == 'Perc' else gcd['btc_market_cap_perc'] * gcd['market_cap_usd']),
                              name='BTC Market Cap', line=dict(color=' #f2a900', width=4)), secondary_y=False)
     fig.add_trace(go.Scatter(x=gcd['timestamp'],
-                             y=(gcd['eth_market_cap_perc'] if yaxis_type == 'Perc' else gcd['eth_market_cap_perc'] * gcd['market_cap_usd']),
+                             y=(gcd['eth_market_cap_perc'] if yaxis_type_mcap == 'Perc' else gcd['eth_market_cap_perc'] * gcd['market_cap_usd']),
                              name='ETH Market Cap', line=dict(color=' #6B8068', width=4)), secondary_y=True)
     fig.update_layout(
         plot_bgcolor='white',
-        title='BTC and ETH Market Caps'
+        title='BTC and ETH Market Caps',
+        legend=dict(
+            yanchor="bottom",
+            y=0.99,
+            xanchor="right",
+            x=0.55)
     )
-    fig.update_yaxes(title_text=f'''BTC Market Cap ({'%' if yaxis_type == 'Perc' else 'USD'})''', secondary_y=False)
-    fig.update_yaxes(title_text=f'''ETH Market Cap ({'%' if yaxis_type == 'Perc' else 'USD'})''', secondary_y=True)
-    fig.update_yaxes(type='linear' if yaxis_type == 'Perc' else 'log')
+    fig.update_yaxes(title_text=f'''BTC Market Cap ({'%' if yaxis_type_mcap == 'Perc' else 'USD'})''', secondary_y=False)
+    fig.update_yaxes(title_text=f'''ETH Market Cap ({'%' if yaxis_type_mcap == 'Perc' else 'USD'})''', secondary_y=True)
+    fig.update_yaxes(type='linear' if yaxis_type_mcap == 'Perc' else 'log')
     return fig
 
 
